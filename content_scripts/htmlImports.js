5705f6c408958646b148a9102f78c2e28a824ec2
Tânia Silva <tdosilva@gmail.com>
2017-09-30 03:41:07 +0100

    All pixelized text

---
+++ content_scripts/htmlImports.js
@@ -0,0 +1,5 @@
+var link = document.createElement('link');
+link.href="https://fonts.googleapis.com/css?family=Press+Start+2P";
+link.rel = "stylesheet";
+
+document.getElementsByTagName('head')[0].appendChild(link);
